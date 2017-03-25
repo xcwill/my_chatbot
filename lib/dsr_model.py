77847afd0a7d7644641c53d67e4fef69546b985b
Neil_Xu <xcwillsnow@gmail.com>
2017-03-25 10:15:10 +0800

    push

---
+++ lib/dsr_model.py
@@ -0,0 +1,17 @@
+#using dsr as a Q-value estimator
+import tensorflow as tf
+import numpy as np
+
+
+class DSR(object):
+
+
+    def __init__(self):
+        pass
+
+    def build_model(self):
+
+
+
+    def train(self):
+        pass
